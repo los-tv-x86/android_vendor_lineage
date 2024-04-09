616abbb472ff9917fcb225fb200ef3cef75caef5
hmtheboy154 <buingoc67@gmail.com>
2024-04-09 15:33:35 -0400

    Clean asm/signal.h to fix building for x86
    Change-Id: Ibf169e42ad3d0c0d15ba4fc5840beb782d42c007
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- tools/clean_headers.sh
+++ tools/clean_headers.sh
@@ -6,3 +6,6 @@ export ANDROID_BUILD_TOP="$PWD"
 ./bionic/libc/kernel/tools/clean_header.py -u \
     "$1/usr/include/asm/signal.h" \
     "$1/usr/include/asm-generic/signal.h"
+
+./bionic/libc/kernel/tools/clean_header.py -u \
+    "$1/usr/include/asm/signal.h"
